5cf42acf4d7653ef724ed1f2c0d3d77e5a4ed7c2
tanupoo <github-shoichi@tanu.org>
2023-05-12 15:33:13 +0900

    added new tests.

--- test.py
+++ test.py
@@ -20,8 +20,8 @@ this is a python snipet.
 ```
     """
     md = ReadMarkdown(text=text)
-    assert md.quotes[0].lang == "python"
     assert md.quotes[0].id == "1"
+    assert md.quotes[0].lang == "python"
 
 def test02():
     text = """
@@ -30,8 +30,75 @@ this is a python snipet.
 ````
     """
     md = ReadMarkdown(text=text)
-    print(md.quotes)
-    assert md.quotes[0].lang == "python"
     assert md.quotes[0].id == "1"
+    assert md.quotes[0].lang == "python"
 
-test02()
+def test_not_snipet01():
+    text = """
+```
+this is a fenced block.
+```
+    """
+    md = ReadMarkdown(text=text)
+    assert md.quotes == []
+
+def test_not_snipet02():
+    text = """
+````
+this is a fenced block.
+````
+    """
+    md = ReadMarkdown(text=text)
+    assert md.quotes == []
+
+def test_not_snipet03():
+    text = """
+````
+```python
+this is a fenced block, not a python snipet,
+```
+````
+    """
+    md = ReadMarkdown(text=text)
+    assert md.quotes == []
+
+def test_tweak01():
+    text = """
+```
+````python
+this is a fenced block, not a python snipet,
+````
+```
+    """
+    md = ReadMarkdown(text=text)
+    assert md.quotes == []
+
+    text = """
+```python
+#%inc: third
+
+if __name__ == "__main__":
+    sample_three()
+
+#%name:main
+```
+
+```python
+#%lib
+#%name:third
+def sample_three():
+    print("three")
+```
+    """
+    md = ReadMarkdown(text=text)
+    assert len(md.quotes) == 2
+    assert md.quotes[0].id == "1"
+    assert md.quotes[0].lang == "python"
+    assert md.quotes[0].name == "main"
+    assert md.quotes[0].lib is False
+    assert md.quotes[1].id == 0
+    assert md.quotes[1].lang == "python"
+    assert md.quotes[1].name == "third"
+    assert md.quotes[1].lib is True
+
+test_tweak01()
